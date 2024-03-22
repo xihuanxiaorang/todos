2bacb5317ed127e7f3518ae068e9b3332884c512
小让 <15019474951@163.com>
2024-03-22 11:19:08 +0800

    feat: 初始化项目

---
+++ src/components/TodoHeader.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts"></script>
+
+<template>
+  <header class="header">
+    <h1>todos</h1>
+    <input class="new-todo" placeholder="What needs to be done?" autofocus />
+  </header>
+</template>
+
+<style scoped></style>
